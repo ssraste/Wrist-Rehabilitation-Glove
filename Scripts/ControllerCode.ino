ffaff40d948d1060f143605e28467c3a9aef424a
Srushti Raste <srushtiraste44@gmail.com>
2025-08-10 18:22:41 -0400

    Add files via upload

---
+++ Scripts/ControllerCode.ino
@@ -0,0 +1,79 @@
+#include <Wire.h>
+#include <MPU6050_light.h>
+#include <Stepper.h>
+
+MPU6050 mpu(Wire);
+
+const float STEPS_PER_REV = 32;     // Amount of gear reduction
+const float GEAR_RED = 64;          // Gear reduction ratio
+const float STEPS_PER_OUT_REV = STEPS_PER_REV * GEAR_RED; // Steps per geared output rotation
+
+// Defines the number of steps per revolution
+const int stepsPerRevolution = STEPS_PER_OUT_REV;
+
+// Creates instances of stepper class
+// Pins entered in sequence IN1–IN3–IN2–IN4 for Stepper
+Stepper myStepper1(stepsPerRevolution, 8, 10, 9, 11);
+Stepper myStepper2(stepsPerRevolution, 4, 6, 5, 7);
+
+char data;
+
+void setup() {
+  Serial.begin(115200); 
+  Wire.begin();
+
+  // Start the serial communication with MPU6050
+  byte status = mpu.begin();
+  Serial.print(F("MPU6050 status: "));
+  Serial.println(status);
+
+  while (status != 0) {
+    // Stop everything if could not connect to MPU6050
+  }
+
+  Serial.println(F("Calculating offsets, do not move MPU6050"));
+  delay(1000);
+  mpu.calcOffsets(); 
+  Serial.println("Done!");
+  // mpu.upsideDownMounting = true; // Uncomment if MPU6050 is mounted upside down
+}
+
+void loop() {
+  if (Serial.available()) {
+    data = Serial.read();
+  }
+
+  if (data == 'u') {
+    // Serial.println("Up");
+    myStepper1.setSpeed(10);
+    myStepper1.step(stepsPerRevolution / 6);
+    // delay(1000);
+  }
+  else if (data == 'd') {
+    myStepper1.setSpeed(10);
+    myStepper1.step(-stepsPerRevolution / 6);
+    // delay(1000);
+  }
+
+  if (data == 'l') {
+    myStepper2.setSpeed(10);
+    myStepper2.step(stepsPerRevolution / 6);
+  }
+  else if (data == 'r') {
+    myStepper2.setSpeed(10);
+    myStepper2.step(-stepsPerRevolution / 6);
+  }
+
+  mpu.update();
+
+  int yaw = map(int(mpu.getAngleZ()), -24, 50, 800, 0);
+  int pitch = map(int(mpu.getAngleY()), -45, 60, 600, 0);
+
+  // Send yaw and pitch to the serial port
+  Serial.print("Yaw: ");
+  Serial.print(yaw);
+  Serial.print(", Pitch: ");
+  Serial.println(pitch);
+
+  delay(100); // Delay to control data rate
+}
